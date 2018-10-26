1909441ecbb37529fe59aa18492bf2cd73a7ec45
RodrigoCruz1154 <00115417@uca.edu.sv>
2018-10-26 09:01:41 -0600

    Listo

--- app/main.js
+++ app/main.js
@@ -17,9 +17,9 @@ mimeTypes = {
 http.createServer((req, res) => {
     var pathname = url.parse(req.url).pathname;
 
-    if(pathname == "/"){
-          pathname = "../index.html";
-        } 
+    if (pathname == "/") {
+        pathname = "../index.html";
+    }
 
     if (pathname == "../index.html") {
         fs.readFile(pathname, (err, data) => {
@@ -49,70 +49,71 @@ http.createServer((req, res) => {
     }
 
     if (req.method === 'POST' && pathname == "/cv") {
-          collectRequestData(req, (err, result) => {
-        
-            if (err) {
-              res.writeHead(400, {
-                'content-type': 'text/html'
-              });
-              return res.end('Bad Request');
-            }
-        
-            fs.readFile("../templates/plantilla.html", function (err, data) {
-              if (err) {
-                console.log(err);
-                // HTTP Status: 404 : NOT FOUND
-                // Content Type: text/plain
-                res.writeHead(404, {
-                  'Content-Type': 'text/html'
-                });
-                return res.end("404 Not Found");
-              }
-        
-              res.writeHead(200, {
-                'Content-Type': mimeTypes[pathname.split('.').pop()] || 'text/html'
-              });
-        
-              //Variables de control.
-        
-              let parsedData = data.toString().replace('${dui}', result.dui)
-                .replace("${lastname}", result.lastname)
-                .replace("${firstname}", result.firstname)
-                .replace("${gender}", result.gender)
-                .replace("${civilStatus}", result.civilStatus)
-                .replace("${birth}", result.birth)
-                .replace("${exp}", result.exp)
-                .replace("${tel}", result.tel)
-                .replace("${std}", result.std);
-        
-              res.write(parsedData);
-              return res.end();
-            });
-        
-          });
-        } 
+        collectRequestData(req, (err, result) => {
 
-    if(pathname.split(".")[1] == "css"){
-          fs.readFile(".."+pathname, (err, data)=>{
-        
-            if (err) {
-              console.log(err);
-              res.writeHead(404, {
-                'Content-Type': 'text/html'
-              });       return res.end("404 Not Found");     }
-        
-            res.writeHead(200, {
-              'Content-Type': mimeTypes[pathname.split('.').pop()] || 'text/css'
-            });
-        
-            // Escribe el contenido de data en el body de la respuesta.
-            res.write(data.toString());
-        
-        
-            // Envia la respuesta
-            return res.end();
-          });
-        } 
+            if (err) {
+                res.writeHead(400, {
+                    'content-type': 'text/html'
+                });
+                return res.end('Bad Request');
+            }
+
+            fs.readFile("../templates/plantilla.html", function (err, data) {
+                if (err) {
+                    console.log(err);
+                    // HTTP Status: 404 : NOT FOUND
+                    // Content Type: text/plain
+                    res.writeHead(404, {
+                        'Content-Type': 'text/html'
+                    });
+                    return res.end("404 Not Found");
+                }
+
+                res.writeHead(200, {
+                    'Content-Type': mimeTypes[pathname.split('.').pop()] || 'text/html'
+                });
+
+                //Variables de control.
+
+                let parsedData = data.toString().replace('${dui}', result.dui)
+                    .replace("${lastname}", result.lastname)
+                    .replace("${firstname}", result.firstname)
+                    .replace("${gender}", result.gender)
+                    .replace("${civilStatus}", result.civilStatus)
+                    .replace("${birth}", result.birth)
+                    .replace("${exp}", result.exp)
+                    .replace("${tel}", result.tel)
+                    .replace("${std}", result.std);
+
+                res.write(parsedData);
+                return res.end();
+            });
+
+        });
+    }
+
+    if (pathname.split(".")[1] == "css") {
+        fs.readFile(".." + pathname, (err, data) => {
+
+            if (err) {
+                console.log(err);
+                res.writeHead(404, {
+                    'Content-Type': 'text/html'
+                }); return res.end("404 Not Found");
+            }
+
+            res.writeHead(200, {
+                'Content-Type': mimeTypes[pathname.split('.').pop()] || 'text/css'
+            });
+
+            // Escribe el contenido de data en el body de la respuesta.
+            res.write(data.toString());
+
+
+            // Envia la respuesta
+            return res.end();
+        });
+    }
 
 }).listen(8081);
 
